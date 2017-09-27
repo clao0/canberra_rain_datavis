9c70f3d2527874f3905199fa480100f7d7d897c0
Ben Swift <ben@benswift.me>
2017-09-27 16:17:52 +1000

    update sketch.js template

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
+var data;
+
+function preload() {
+    data = loadJSON(/* which dataset will you choose? */);
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    // this line is required due to a bug in the current version of p5
+    // https://github.com/processing/p5.js/issues/2154
+    data = Object.values(data);
     // any additional setup code goes here
 }
 
